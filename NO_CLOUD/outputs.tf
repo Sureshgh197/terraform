4259ec5002fb71a6ba3f170bec4c7b1d9f8cc660
Naresh Batta <battanaresh456@gmail.com>
2026-03-09 19:12:58 +0530

    ...

--- NO_CLOUD/outputs.tf
+++ NO_CLOUD/outputs.tf
@@ -1,3 +1,15 @@
 output "age" {
   value = "My rand age is ${random_integer.rand_num.result}"
 }
+
+output "fname" {
+    value = "My var fname is ${upper(var.fname)}"
+}
+
+output "fruits" {
+  value = join("\n", [for fruit in var.fruits : "My favourite fruit is ${fruit}"])
+}
+
+output "capitals" {
+    value = [for cntr, cap in var.capitals: "The cap of ${cntr} is ${cap}"]
+}
--- NO_CLOUD/variables.tf
+++ NO_CLOUD/variables.tf
@@ -3,3 +3,27 @@ variable "name" {
   type        = string
   default     = "suresh"
 }  
+
+variable "fname" {
+  description = "what is ur name"
+  type        = string
+}  
+
+#usage of list
+#for output blocks we cannot use for_each, but use with for loop
+variable "fruits" {
+  description = "my list of fruits"
+  type        = list(string)
+  default     = ["apple", "banana", "grapes"]
+}
+
+#usage of map
+variable "capitals" {
+  description = "my map of capitals"
+  type        = map(string)
+  default     = {
+    "india" = "delhi",
+    "usa"   = "washington",
+    "uk"    = "london"
+  }
+}
